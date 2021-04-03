7136ea9224b40233c13ba4871f9397969e610348
Josh <zylo.codes@gmail.com>
2021-04-03 14:49:10 -0500

    Fix Knob render bug

--- src/components/volume.js
+++ src/components/volume.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Donut } from 'react-dial-knob'
 import { useAppContext } from '../context'
 
 export default function Volume() {
   const { state, dispatch } = useAppContext()
 
+  const isFirstRender = useRef(true)
+
   return <Donut
     diameter={200}
     min={-12}
     max={12}
     step={1}
-    value={state.volume || 0}
-    onValueChange={newVolume => dispatch({ type: 'CHANGE_VOLUME', payload: newVolume })}
+    value={state.volume}
+    onValueChange={newVolume => {
+      !isFirstRender.current
+        ? dispatch({ type: 'CHANGE_VOLUME', payload: newVolume })
+        : isFirstRender.current = false
+    }}
     ariaLabelledBy="volume"
   >
     <label id="volume">volume</label>
